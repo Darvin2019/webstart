7eb6f9fa3c9f56519e640efd0c212af65e22ab15
Денис Пивиков <pivikovdenis@yandex.ru>
2019-03-10 11:52:55 +0300

    Сделана валидность форм, наложена маска

---
+++ src/js/validate.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  $('#brif-form').validate({
+    rules: {
+      username: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      phone:"required",
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      username: "Заполните поле",
+      phone: "Заполните поле",
+      email: {
+        required: "Заполните поле",
+        email: "Введите корректный email"
+      }
+    }
+  });
+  $('#offer-form').validate({
+    rules: {
+      username: {
+        required: true,
+        minlength: 2
+      },
+      phone: {
+        required: true
+      }
+    },
+    messages: {
+      username: "Заполните поле",
+      phone: {
+        required: "Заполните поле"
+      }
+    }
+  });
+})
